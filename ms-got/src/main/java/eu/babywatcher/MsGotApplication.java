0b3c8147274c2f66002210bdca7bf3b2bb7d807c
Rünno Ruul <rynno.ruul@telia.ee>
2018-10-05 14:24:27 +0300

    new project

---
+++ ms-got/src/main/java/eu/babywatcher/MsGotApplication.java
@@ -0,0 +1,12 @@
+package eu.babywatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGotApplication.class, args);
+	}
+}
